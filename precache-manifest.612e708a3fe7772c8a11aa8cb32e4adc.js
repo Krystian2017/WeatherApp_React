191c37372cc4cf4e76ab206fa44cc601081ef94b
Krystian Bieniek <krystekk2013@gmail.com>
2019-10-16 18:04:59 +0200

    Updates

---
+++ precache-manifest.612e708a3fe7772c8a11aa8cb32e4adc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6660d341eb1c8077618",
+    "url": "/WeatherApp_React/static/js/runtime~main.b6660d34.js"
+  },
+  {
+    "revision": "f4425a0b45e003d081ab",
+    "url": "/WeatherApp_React/static/js/main.f4425a0b.chunk.js"
+  },
+  {
+    "revision": "2eb1efbf424eb56e824f",
+    "url": "/WeatherApp_React/static/js/1.2eb1efbf.chunk.js"
+  },
+  {
+    "revision": "f4425a0b45e003d081ab",
+    "url": "/WeatherApp_React/static/css/main.7c391ed6.chunk.css"
+  },
+  {
+    "revision": "c0dd271cd98e43d7a4586f30e94d318a",
+    "url": "/WeatherApp_React/index.html"
+  }
+];
